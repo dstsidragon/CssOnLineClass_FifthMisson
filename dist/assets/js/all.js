ce5af9576a0b4a82942d59bfe06b60f47e380546
Lucien <sim322on@yahoo.com.tw>
2020-11-29 01:47:07 +0800

    add model

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -1,7 +1,6 @@
+// console.log('Hello!');
+// $(document).ready(() => {
+//   console.log(' Hello!');
+// });
 "use strict";
-
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
-});
 //# sourceMappingURL=all.js.map
