367aa75dde117f7ac006c9787645981e3ce54346
Lucien <sim322on@yahoo.com.tw>
2020-12-01 00:40:20 +0800

    edit rwd grid

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,6 +1,32 @@
-// console.log('Hello!');
 
-// $(document).ready(() => {
-//   console.log(' Hello!');
-// });
+/* Question */
+$(function () {
+    /*開啟VIP加速*/ 
+    $('.btn-loading').click(function () {
+        //開啟/關閉單獨loading
+        $('.loading-ninja').toggleClass('d-none');
+        //開啟/關閉八倍loading
+        $('.loadingGroup').toggleClass('d-none');
+        //開啟/關閉八倍loading按鈕
+        $('.btn-loading').toggleClass('d-none');
+        //開啟/關閉八倍loading按鈕
+        $('.btn-cancelLoading').toggleClass('d-none');
 
+    })
+    /*關閉VIP加速*/ 
+    $('.btn-cancelLoading').click(function () {
+        //開啟/關閉單獨loading
+        $('.loading-ninja').toggleClass('d-none');
+        //開啟/關閉八倍loading
+        $('.loadingGroup').toggleClass('d-none');
+        //開啟/關閉八倍loading按鈕
+        $('.btn-loading').toggleClass('d-none');
+        //開啟/關閉八倍loading按鈕
+        $('.btn-cancelLoading').toggleClass('d-none');
+
+        
+    })
+    
+
+    
+});
